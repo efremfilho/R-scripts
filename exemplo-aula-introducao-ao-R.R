975f842e5c218adac68c36500ef2bb1597480b30
Éfrem Maranhão Filho <efremdx@yahoo.com.br>
2018-05-06 18:34:57 -0500

    cria arquivo de script da aula

---
+++ exemplo-aula-introducao-ao-R.R
@@ -0,0 +1,21 @@
+# Atribuição de Objetos (declaração de variáveis)
+a <- 1:5 # Para colocar '<-' com atalho do teclado basta 'alt + -'
+a
+a <- 4
+b <- 2
+c <- a*b+2
+c
+t <- 2^2 
+t
+
+# Instalando pacotes
+install.packages("psych")
+library("psych")
+
+# Organizando o Working Directory
+getwd()
+setwd("/home/efremfilho")
+
+# Instalar o Rcommander
+install.packages("Rcmdr")
+
